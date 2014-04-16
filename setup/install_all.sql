8db1f8f7d594f6511a4b330d2b6f1419a0e644b0
emorkle <emorkle@E7B499BAECEA41.ericsson.se>
2014-04-16 14:59:22 +0200

    Added ZIP_UTIL_PKG as library and included in install script.

--- setup/install_all.sql
+++ setup/install_all.sql
@@ -1,5 +1,8 @@
-@@../ora/apexir_xlsx_types_pkg.pks
+@@../ora/zip_util_pkg.pks
 @@../ora/xlsx_builder_pkg.pks
-@@../ora/xlsx_builder_pkg.pkb
+@@../ora/apexir_xlsx_types_pkg.pks
 @@../ora/apexir_xlsx_pkg.pks
+
+@@../ora/zip_util_pkg.pkb
+@@../ora/xlsx_builder_pkg.pkb
 @@../ora/apexir_xlsx_pkg.pkb
